13e75768d0f4073e771ca20aa4de65ca039ac778
szana8 <szana8@gmail.com>
2024-05-21 21:17:00 +0200

    Add sanctum

--- api/routes/web.php
+++ api/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Authenticate\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('login', [LoginController::class, 'authenticate'])->name('login');
