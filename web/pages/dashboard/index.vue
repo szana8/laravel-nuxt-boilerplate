c437e36fe0d6a4d63b7d188b0f631088a1e81aa4
szana8 <szana8@gmail.com>
2024-09-02 21:50:18 +0200

    WIP

--- web/pages/dashboard/index.vue
+++ web/pages/dashboard/index.vue
@@ -3,15 +3,13 @@
         <h1 class="font-bold text-2xl text-center mt-6">Dashboard</h1>
         <div>Logged in: {{ loggedIn }}</div>
         <div v-if="true">User: {{ user }}</div>
-        <div v-if="loggedIn">Session: {{ session.token }}</div>
+        <div v-if="loggedIn">Session: {{ session }}</div>
         <button v-if="loggedIn" @click="clear">Logout</button>
-
-        {{ data }}
     </div>
 </template>
 
 <script setup lang="ts">
 const { loggedIn, user, session, fetch, clear } = useUserSession()
 
-const { data } = await useCustomFetch<[]>('/api/user')
+//const data = await useCustomFetch<[]>('/api/user')
 </script>
